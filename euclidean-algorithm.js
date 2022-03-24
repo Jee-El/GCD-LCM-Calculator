14ba79d3eee4057e5b25546138565a05c9908542
Lhoussaine Ghallou <ghallouhoussaine47@gmail.com>
2022-03-24 23:27:13 +0100

    Change a comment from single-line to multi-line

--- euclidean-algorithm.js
+++ euclidean-algorithm.js
@@ -11,7 +11,10 @@ function gcd(a, b) {
     if (isNaN(a * b) || (a < 0 && b < 0) || (a == 0 && b == 0) ){
         return `arguments = (a, b) != (0, 0) where both a and b are not negative at the same time`;
     } else if (b === 0) {
-        // JS's definition of modulo is different than what we're taught at school, this is to make it align with what we were taught
+        /*
+        JS's definition of modulo is different than what we're taught at school,
+        this is to make it align with what we were taught
+        */
         return a > 0 ? a : -a;
     } else if (a === 0) {
         // 0 % x = 0 but the positive value of x is the GCD
