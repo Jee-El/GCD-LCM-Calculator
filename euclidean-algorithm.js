f0c25b7de74d00d22f89f1179882bd28d12fac03
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-03-30 00:57:58 +0000

    Fix issues with if statement conditions
    Now the function will check whether or not the arguments are numbers using Number.isInteger(), instead of isNaN(), while also checking whether they are integers or not.

--- euclidean-algorithm.js
+++ euclidean-algorithm.js
@@ -1,27 +1,26 @@
-// there is probably a simpler method to implement Euclide's algorithm but it's 00am and i'm happy that at least it seems to work well, i'll simplify it later
+// GCD stands for Greatest Common Divisor
 
-// gcd stands for Greatest Common Divisor
+// Here modulo is defined as : a = r [b] <=> b / a - r . But the modulo operator in js acts differently because, to it, `a % b` means DIVIDE a by b. Since dividing by 0 gets us nowhere, `a % 0` would return NaN. Meanwhile, if we follow the first definition, a = r [0] is true and makes sense, although r would have to be equal to a. So I wrote an else..if statement to make a % 0 = |a|.
 
-// here modulo is defined as : x = y [n] <=> n / x - y
-// the forward slash meaning "divides"
+// The forward slash meaning "divides".
 
-function gcd(a, b) {
+function GCD(a, b) {
 
-    // check if a & b are numbers or check if both are negative or check if both are null
-    if (isNaN(a * b) || (a < 0 && b < 0) || (a == 0 && b == 0) ){
-        return `arguments = (a, b) != (0, 0) where either of arguments is positive`;
+    // Check if a & b are integers, positive, not null.
+    if ( !Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0 || (a == 0 && b == 0) ) {
+        return `the arguments, a & b, should be positive integers and at least one of them is not null`;
     } else if (b === 0) {
         /*
         JS's definition of modulo is different than what we're taught at school,
-        this is to make it align with what we were taught
+        this is to make it align with that.
         */
         return a > 0 ? a : -a;
     } else if (a === 0) {
-        // 0 % x = 0 but the absolute value of x is the GCD
+        // 0 % x = 0 but the absolute value of x is the GCD.
         return b > 0 ? b : -b;
     } else if (a % b === 0) {
-        // to end Euclide's algorithm
+        // To end Euclide's algorithm.
         return a > b ? b : a;
     }
-    return gcd(b, a % b);
+    return GCD(b, a % b);
 }
