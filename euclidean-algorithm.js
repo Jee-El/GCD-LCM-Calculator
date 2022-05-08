44cfece8aa5bba08176d1f58b1f64fba6264750b
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-05-08 23:49:26 +0100

    Feat: Add option to calculate LCM
    LCM being the Lowest Common Multiple.

--- euclidean-algorithm.js
+++ euclidean-algorithm.js
@@ -9,57 +9,67 @@ although r would have to be equal to a. So I wrote an else..if statement to make
 
 const result = document.querySelector('p');
 const input = document.querySelectorAll('input');
-input[0].focus();
-const submit = document.querySelector('button[type="submit"]');
-const reset = document.querySelector('button[type="reset"]');
+const GCDBtn = document.querySelector('.GCD-button');
+const LCMBtn = document.querySelector('.LCM-button');
+const resetBtn = document.querySelector('button[type="reset"]');
 
-submit.addEventListener('click', () => {
+// Get GCD when GCD button is clicked
+GCDBtn.addEventListener('click', (e) => {
     result.classList.remove('greyed-out');
     if (input[0].value.trim() === '' || input[1].value.trim() === '') {
         return result.textContent = `Please fill both the integer fields`;
     }
-    checkForValidInput(+input[0].value, +input[1].value);
+    checkForValidInput(+input[0].value, +input[1].value, e);
 });
 
-document.addEventListener('keydown', (e) => {
-    if (e.keyCode !== 13) return;
+// Get LCM when LCM button is clicked
+LCMBtn.addEventListener('click', (e) => {
     result.classList.remove('greyed-out');
     if (input[0].value.trim() === '' || input[1].value.trim() === '') {
         return result.textContent = `Please fill both the integer fields`;
     }
-    checkForValidInput(+input[0].value, +input[1].value);
+    checkForValidInput(+input[0].value, +input[1].value, e);
 });
 
-reset.addEventListener('click', () => {
+// Clear input
+resetBtn.addEventListener('click', () => {
     input[0].value = '';
     input[1].value = '';
-    result.textContent = 'The GCD is shown here!';
+    result.textContent = 'The GCD/LCM is shown here!';
     result.classList.add('greyed-out');
-})
+});
 
-function checkForValidInput(a, b) {
+// check input, then call the function tied to the clicked button
+function checkForValidInput(a, b, e) {
     // Check if a & b are integers, both not null.
     if ( !Number.isSafeInteger(a) || !Number.isSafeInteger(b)) {
-        return result.textContent = `Input must be two integers, each between -(2^53 - 1) and (2^53 - 1)`;
+        return result.textContent = `Input must be two integers between -(2^53 - 1) and (2^53 - 1)`;
     }
     if ( a === 0 && b === 0) {
         return result.textContent = `At least one integer should be non-null`;
     }
-
-    return findGCD(a, b);
+    if (e.target.classList.contains('GCD-button')) return findGCD(a, b);
+    if (e.target.classList.contains('LCM-button')) return findLCM(a, b);
 }
 
 function findGCD(a, b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
     // 0 % b = 0 but the absolute value of b is the GCD.
-    if (a === 0) return result.textContent = b > 0 ? b : -b;
+    if (a === 0) return result.textContent = b;
 
     // Check the second comment above for an explanation of this one.
     if (b === 0) return findGCD(b, a);
 
     // To end Euclide's algorithm.
     if (a % b === 0) {
-        return result.textContent = Math.abs(a) > Math.abs(b) ? Math.abs(b) : Math.abs(a);
+        return result.textContent = a > b ? b : a;
     }
-    
+
     return findGCD(b, a % b);
+}
+
+function findLCM(a, b) {
+    let GCD = findGCD(a, b);
+    return result.textContent = (a * b) / GCD;
 }
